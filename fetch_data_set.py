9490d454c1fed7d7c5826b3d23da7d68d2a244db
Per Segersten <persegersten@gmail.com>
2025-06-18 07:17:15 +0200

    Fetch data set and save it locally

--- fetch_data_set.py
+++ fetch_data_set.py
@@ -1,3 +1,7 @@
+# Fetch data set from: https://archive.ics.uci.edu/dataset/20/census+income
+# and save it to the file system
+
+import pandas as pd
 from ucimlrepo import fetch_ucirepo
 
 # fetch dataset
@@ -12,3 +16,9 @@ print(census_income.metadata)
 
 # variable information
 print(census_income.variables)
+
+# Combine features + target in same DataFrame
+df = pd.concat([X, y], axis=1)
+
+# Save to file
+df.to_csv("data/census_income.csv", index=False)
